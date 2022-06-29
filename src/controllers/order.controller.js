49d15cbb9cdb7982887fcd40c62195eb65a9fe36
vadivelan1998 <vadivelan1998@gmail.com>
2022-06-29 19:12:19 +0530

    controllers added

---
+++ src/controllers/order.controller.js
@@ -0,0 +1,14 @@
+const authorization = require('../middlewares/authorization');
+const Order = require('../models/order.model');
+const { postItem, getAllOrders } = require('./crud.controller');
+
+const router = require('express').Router();
+
+router.post("/",authorization, postItem(Order));
+
+router.get("/", authorization, getAllOrders(Order));
+
+module.exports = router;
+
+
+
