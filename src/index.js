352a0b20cf628f7379c1873f60684327491a94c0
vadivelan1998 <vadivelan1998@gmail.com>
2022-06-29 19:14:52 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,5 +9,35 @@ app.use(cors());
 
 
 
+const { SignUp, SignIn } = require("./controllers/auth.controller");
+
+const productController = require("./controllers/product.controller");
+const favouriteController = require("./controllers/favourite.controller");
+const cartController = require("./controllers/cart.controller");
+const amountController = require("./controllers/amount.controller");
+const couponController = require("./controllers/coupon.controller");
+const addressController = require("./controllers/address.controller");
+const orderController = require("./controllers/order.controller");
+
+app.post("/signup", SignUp);
+
+app.post("/signin", SignIn);
+
+app.use("/products", productController);
+
+app.use("/favourite", favouriteController);
+
+app.use("/cart", cartController);
+
+app.use("/amount", amountController);
+
+app.use("/coupon", couponController);
+
+app.use("/address", addressController);
+
+app.use("/orders", orderController);
+
+
+
 
 module.exports = app;
