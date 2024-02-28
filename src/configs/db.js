20b16dd3861ac13d199e21f4a6d306ca0c577f1a
Vadivelan <97453410+vadivelan1998@users.noreply.github.com>
2024-02-28 12:22:38 +0530

    Update db.js

--- src/configs/db.js
+++ src/configs/db.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 
 const connectMongoDB = () => {
   return mongoose.connect(
-    `mongodb+srv://${process.env.MONGO_USER_NAME}:${process.env.MONGO_PASSWORD}@cluster0.jejpkww.mongodb.net/e_kart?retryWrites=true&w=majority`
+    `mongodb+srv://vadivelan1998:vadivelan1234@cluster0.jejpkww.mongodb.net/e_kart?retryWrites=true&w=majority`
   );
 };
 module.exports = connectMongoDB;
